4916c9893981cb3e78f8ddb3628be7c80694892e
madhusudan <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>
2026-01-17 10:44:42 +0530

    save changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
+// import GoogleTagManager from "./GoogleTagManager";
+// import GoogleTagManagerNoScript from "./GoogleTagManagerNoScript";
 
 const LocalBusiness = {
   "@context": "https://schema.org",
@@ -49,6 +51,7 @@ const LocalBusiness = {
   ]
 
 };
+const GTM_ID = "GTM-M6QT7LCW";
 
 export default function Document() {
 
@@ -82,8 +85,34 @@ export default function Document() {
             __html: JSON.stringify(LocalBusiness),
           }}
         />
+        <Script
+          id="gtm-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+                  (function(w,d,s,l,i){w[l]=w[l]||[];
+                  w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                  var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                  j.async=true;
+                  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                  f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${GTM_ID}');
+                `,
+          }}
+        />
+        {/* <GoogleTagManager /> */}
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* <GoogleTagManagerNoScript /> */}
         <Main />
         <NextScript />
       </body>
