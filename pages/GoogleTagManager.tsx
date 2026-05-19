7497287a2e21c59520e66d5daeaa5b117e653658
Madhusudan Dayal <madhusudan@comfygen.com>
2026-05-19 18:15:53 +0530

    save

--- pages/GoogleTagManager.tsx
+++ pages/GoogleTagManager.tsx
@@ -1,35 +1,17 @@
-
-
 import { useEffect } from "react";
 
-// const GTM_ID = "GTM-M6QT7LCW";
-
 const GTM_ID = "GTM-5K96GNBB";
-
-
-
-
 export default function GoogleTagManager() {
   useEffect(() => {
-    let isLoaded = false;
-
     const loadGTM = () => {
-      if (isLoaded) return;
-      isLoaded = true;
-
       const script = document.createElement("script");
       script.src = `https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`;
       script.async = true;
       document.head.appendChild(script);
     };
 
-    window.addEventListener("scroll", loadGTM);
-    window.addEventListener("click", loadGTM);
-
-    return () => {
-      window.removeEventListener("scroll", loadGTM);
-      window.removeEventListener("click", loadGTM);
-    };
+    window.addEventListener("scroll", loadGTM, { once: true });
+    window.addEventListener("click", loadGTM, { once: true });
   }, []);
 
   return null;
