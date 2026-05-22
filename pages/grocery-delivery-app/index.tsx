0d3ab5a578d7fed68b8ea041927e144ba106722f
Madhusudan Dayal <madhusudan@comfygen.com>
2026-05-22 14:21:19 +0530

    save

--- pages/grocery-delivery-app/index.tsx
+++ pages/grocery-delivery-app/index.tsx
@@ -5,7 +5,7 @@ import TopNavbar from "../../components/Landing/TopNavbar"
 import Hero from '../../components/Landing/Hero'
 import BrandsBar from '../../components/Landing/BrandsBar'
 import Deliverables from '../../components/Landing/Deliverables'
-import AppScreens from '../../components/Landing/AppScreens'
+import TrustBar from '../../components/Landing/TrustBar'
 import AppTypes from '../../components/Landing/AppTypes'
 import TechStack from '../../components/Landing/TechStack'
 import Steps from '../../components/Landing/Steps'
@@ -28,7 +28,8 @@ const index = () => {
       <div className='mt-12'>
         <Hero AppType={JSON_DATA?.AppType} DATA={JSON_DATA.HeroContent} />
       </div>
-      <BrandsBar DATA={JSON_DATA.BrandBar} />
+      <TrustBar DATA={JSON_DATA.reviewed} />
+      {/* <BrandsBar DATA={JSON_DATA.BrandBar} /> */}
       <Deliverables DATA={JSON_DATA.Deliverables} />
       {/* <AppScreens /> */}
       <FoodDeliveryUISection DATA={JSON_DATA.FoodDelivery} />
