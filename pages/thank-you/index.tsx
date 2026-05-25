c4b22111e5791ea041cfcfa05e7992b48ca8e102
Madhusudan Dayal <madhusudan@comfygen.com>
2026-05-25 15:45:54 +0530

    save

--- pages/thank-you/index.tsx
+++ pages/thank-you/index.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ThankYou from '../../components/Landing/ThankYou'
 import Header from '../../components/Landing/Header'
 import Footer from '../../components/Landing/Footer'
+import Head from 'next/head'
 
 const index = () => {
     return (
         <div>
+            <Head>
+                 <meta name="robots" content="noindex, nofollow" />
+            </Head>
             <Header />
             <div className='mt-16'>
                 <ThankYou />
